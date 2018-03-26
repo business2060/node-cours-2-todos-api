3a5e627ff490999fa6990833aa79fa89be0816c3
Hedi Hsouna <business2060@gmail.com>
2018-03-26 17:14:03 +0200

    Add GET /users/me and authentification middleware

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
 var {User}=require('./models/user');
+var {authenticate}=require('./middleware/authenticate');
 
 // var newTodo= new Todo({
 //   text: 'cook dinner'
@@ -119,6 +120,28 @@ Todo.findByIdAndUpdate(id, {$set: body},{new:true}).then((todo)=>{
 
 });
 
+
+app.post('/users',(req,res)=>{
+  var body=_.pick(req.body,['email','password']);
+  var user = new User(body);
+
+  user.save().then(()=>{
+    return user.generateAuthToken();
+    //res.send(user);
+  }).then((token)=>{
+    res.header('x-auth',token).send(user);
+  }).catch((e)=>{
+    res.status(400).send(e);
+  })
+})
+
+
+
+
+app.get('/users/me',authenticate,(req,res)=>{
+  res.send(req.user);
+
+});
 
 app.listen(port,()=>{
   console.log(`Started on port ${port}`);
